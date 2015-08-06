99ec295ff967953d9c143bdbe659f2c394c29ff7
Luis Lourenço <lhal07@gmail.com>
2015-08-06 11:47:18 -0300

    Get server ip and port from arguments of send_message

--- src/libsimplesocket.c
+++ src/libsimplesocket.c
@@ -1,42 +1,39 @@
 #include "simplesocket.h"
 
 
-//#define DEBUG
-#define PORT 11000
-#define SERVERIP "10.1.1.193"
 #define DEBUG
 
-int envia_log(char* param){
+int send_message(char* server_ip, int port, char* message)
+{
 
   int sock_descr;
   struct sockaddr_in server_addr;
   //struct hostent *he;
   struct in_addr ipv4addr;
-  char *URL = "this is another test <EOF>";
 
-  inet_pton(AF_INET, SERVERIP, &ipv4addr);
+  inet_pton(AF_INET, server_ip, &ipv4addr);
   //he = gethostbyaddr(&ipv4addr, sizeof ipv4addr, AF_INET);
 
   //bcopy((char *)he->h_addr, (char *)&server_addr.sin_addr, he->h_length);
   server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(PORT);
+  server_addr.sin_port = htons(port);
 
   if((sock_descr=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     perror("ERROR: Could not open socket");
-    exit(1);
+    return(1);
   }
 
   if(connect(sock_descr, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
     perror("ERROR: Could not connect to server");
-    exit(1);
+    return(1);
   }
 
-  if(write(sock_descr, URL, strlen(URL)) != strlen(URL)){
-    perror("ERROR: Could not send URL");
-    exit(1);
+  if(write(sock_descr, message, strlen(message)) != strlen(message)){
+    perror("ERROR: Could not send message");
+    return(1);
   }
 
-  printf("Sending: %s\n",URL);
+  printf("Sending: %s\n",message);
 
 
   // Tell application that will not send anymore.
@@ -47,10 +44,10 @@ int envia_log(char* param){
   char buffer[BUFSIZ+1];
 
   read(sock_descr, buffer, BUFSIZ);
-  printf("%s\n", buffer);
+  printf("Received: %s\n", buffer);
 
 #endif
 
   close(sock_descr);
-  exit(0);
+  return(0);
 }
